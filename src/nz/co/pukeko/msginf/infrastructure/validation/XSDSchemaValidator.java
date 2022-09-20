36b8b4305053b5b1883b18b0e1211041ecbb6567
Alisdair Hamblyn <ahamblyn@gmail.com>
2022-09-21 04:02:07 +1200

    Junit5

--- src/nz/co/pukeko/msginf/infrastructure/validation/XSDSchemaValidator.java
+++ src/nz/co/pukeko/msginf/infrastructure/validation/XSDSchemaValidator.java
@@ -1,6 +1,7 @@
 package nz.co.pukeko.msginf.infrastructure.validation;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 
 import nz.co.pukeko.msginf.infrastructure.exception.InvalidXMLSchemaException;
@@ -14,6 +15,10 @@ import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 /**
  * This class validates the XML schema against an XSD file. Each MessageController
@@ -89,6 +94,20 @@ public class XSDSchemaValidator {
            xmlData = xmlHeader + xmlData;
        }
       parseXml(xmlData);
+   }
+
+   public static boolean validateXML(String xsdPath, String xmlPath) {
+      try {
+         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+         factory.setErrorHandler(new SAXParserErrorHandler());
+         Schema schema = factory.newSchema(new File(xsdPath));
+         Validator validator = schema.newValidator();
+         validator.validate(new StreamSource(new File(xmlPath)));
+      } catch (IOException | SAXException e) {
+         System.out.println("Exception: "+e.getMessage());
+         return false;
+      }
+      return true;
    }
 
    private void parseXml(String xml) throws MessageException {
