f464a64824e36c292d523043c2ad71f7147d9cb8
Alisdair Hamblyn <ahamblyn@gmail.com>
2024-04-28 10:08:11 +1200

    Publish subscribe

--- src/test/java/nz/co/pukekocorp/msginf/client/adapter/activemq/TestPublishSubscribe.java
+++ src/test/java/nz/co/pukekocorp/msginf/client/adapter/activemq/TestPublishSubscribe.java
@@ -4,14 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import nz.co.pukekocorp.msginf.MessageInfrastructureApplication;
 import nz.co.pukekocorp.msginf.client.adapter.Messenger;
 import nz.co.pukekocorp.msginf.client.adapter.TestUtil;
+import nz.co.pukekocorp.msginf.client.listener.Subscriber;
 import nz.co.pukekocorp.msginf.infrastructure.data.StatisticsCollector;
 import nz.co.pukekocorp.msginf.infrastructure.exception.MessageException;
+import nz.co.pukekocorp.msginf.infrastructure.properties.MessageInfrastructurePropertiesFileParser;
 import nz.co.pukekocorp.msginf.models.message.MessageRequestType;
 import nz.co.pukekocorp.msginf.models.message.MessageResponse;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +31,25 @@ public class TestPublishSubscribe {
     @Autowired
     private Messenger messenger;
 
+    private static List<Subscriber> subscriberList;
+
+    @BeforeAll
+    public static void setUp() {
+        try {
+            MessageInfrastructurePropertiesFileParser parser = new MessageInfrastructurePropertiesFileParser();
+            subscriberList = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                var subscriber = new Subscriber((i + 1), parser, "activemq_pubsub",
+                        "TopicConnectionFactory", "TestTopic",
+                         "tcp://localhost:61616");
+                subscriber.run();
+                subscriberList.add(subscriber);
+            }
+        } catch (MessageException e) {
+            log.error("Unable to setup TestPublishSubscribe test", e);
+        }
+    }
+
     @AfterAll
     public static void tearDown() {
         // Sleep so messages finish processing before shutdown
@@ -33,6 +57,7 @@ public class TestPublishSubscribe {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
         }
+        subscriberList.forEach(Subscriber::shutdown);
     }
 
     @Test
