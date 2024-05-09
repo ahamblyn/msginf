58f070eff53435eb3575a19819d6a0c9f3a4240b
Alisdair Hamblyn <ahamblyn@gmail.com>
2024-05-09 16:22:43 +1200

    Unit tests

--- src/main/java/nz/co/pukekocorp/msginf/services/MessageService.java
+++ src/main/java/nz/co/pukekocorp/msginf/services/MessageService.java
@@ -3,8 +3,10 @@ package nz.co.pukekocorp.msginf.services;
 import lombok.extern.slf4j.Slf4j;
 import nz.co.pukekocorp.msginf.client.adapter.Messenger;
 import nz.co.pukekocorp.msginf.infrastructure.exception.MessageException;
+import nz.co.pukekocorp.msginf.infrastructure.properties.MessageInfrastructurePropertiesFileParser;
 import nz.co.pukekocorp.msginf.infrastructure.util.Util;
 import nz.co.pukekocorp.msginf.models.configuration.MessageProperty;
+import nz.co.pukekocorp.msginf.models.configuration.MessagingModel;
 import nz.co.pukekocorp.msginf.models.message.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,6 +42,7 @@ public class MessageService implements IMessageService {
     public Optional<RestMessageResponse> submit(RestMessageRequest payload) {
         String transactionId = UUID.randomUUID().toString();
         try {
+            validateMessagingModel("submit", payload.messageSystem());
             Instant start = Instant.now();
             MessageRequest messageRequest = new MessageRequest(MessageRequestType.SUBMIT, payload.messageConnector(), transactionId);
             if (payload.binaryMessage() != null && !payload.binaryMessage().isEmpty()) {
@@ -52,7 +55,7 @@ public class MessageService implements IMessageService {
             return Optional.of(new RestMessageResponse("Message submitted successfully", transactionId, TransactionStatus.SUCCESS, duration));
         } catch (MessageException e) {
             log.error("Unable to submit the message", e);
-            return Optional.of(new RestMessageResponse(e.getMessage(), transactionId, TransactionStatus.FAILURE));
+            return Optional.of(new RestMessageResponse(e.getMessage(), transactionId, TransactionStatus.ERROR));
         }
     }
 
@@ -66,13 +69,14 @@ public class MessageService implements IMessageService {
     @Override
     public List<RestMessageResponse> receiveMessages(String messagingSystem, String messageConnector, long timeout) {
         try {
+            validateMessagingModel("receiveMessages", messagingSystem);
             List<MessageResponse> messages = messenger.receiveMessages(messagingSystem, messageConnector, timeout);
             return messages.stream().map(m -> new RestMessageResponse("Received message", m.getTextResponse(),
                     Util.encodeBinaryMessage(m.getBinaryResponse()), UUID.randomUUID().toString(),
                     TransactionStatus.SUCCESS, 0L)).toList();
         } catch (MessageException e) {
             log.error("Unable to receive the messages", e);
-            return Collections.singletonList(new RestMessageResponse(e.getMessage(), UUID.randomUUID().toString(), TransactionStatus.FAILURE));
+            return Collections.singletonList(new RestMessageResponse(e.getMessage(), UUID.randomUUID().toString(), TransactionStatus.ERROR));
         }
     }
 
@@ -85,6 +89,7 @@ public class MessageService implements IMessageService {
     public Optional<RestMessageResponse> requestReply(RestMessageRequest payload) {
         String transactionId = UUID.randomUUID().toString();
         try {
+            validateMessagingModel("requestReply", payload.messageSystem());
             Instant start = Instant.now();
             MessageRequest messageRequest = new MessageRequest(MessageRequestType.REQUEST_RESPONSE, payload.messageConnector(), transactionId);
             if (payload.binaryMessage() != null && !payload.binaryMessage().isEmpty()) {
@@ -101,7 +106,7 @@ public class MessageService implements IMessageService {
             return Optional.of(restMessageResponse);
         } catch (MessageException e) {
             log.error("Unable to run requestReply", e);
-            return Optional.of(new RestMessageResponse(e.getMessage(), transactionId, TransactionStatus.FAILURE));
+            return Optional.of(new RestMessageResponse(e.getMessage(), transactionId, TransactionStatus.ERROR));
         }
     }
 
@@ -114,6 +119,7 @@ public class MessageService implements IMessageService {
     public Optional<RestMessageResponse> publish(RestMessageRequest payload) {
         String transactionId = UUID.randomUUID().toString();
         try {
+            validateMessagingModel("publish", payload.messageSystem());
             Instant start = Instant.now();
             MessageRequest messageRequest = new MessageRequest(MessageRequestType.PUBLISH_SUBSCRIBE, payload.messageConnector(), transactionId);
             if (payload.binaryMessage() != null && !payload.binaryMessage().isEmpty()) {
@@ -126,7 +132,7 @@ public class MessageService implements IMessageService {
             return Optional.of(new RestMessageResponse("Message published successfully", transactionId, TransactionStatus.SUCCESS, duration));
         } catch (MessageException e) {
             log.error("Unable to publish the message", e);
-            return Optional.of(new RestMessageResponse(e.getMessage(), transactionId, TransactionStatus.FAILURE));
+            return Optional.of(new RestMessageResponse(e.getMessage(), transactionId, TransactionStatus.ERROR));
         }
     }
 
@@ -137,5 +143,27 @@ public class MessageService implements IMessageService {
                     new MessageProperty(property.name(), property.value())).toList();
         }
         return messageProperties;
+    }
+
+    /**
+     * The publish service method can only be called by the publish-subscribe messaging model.
+     * The other service methods can only be called by the point-to-point messaging model.
+     * @param serviceMethod the service method e.g. submit, requestReply, publish etc
+     * @param messagingSystem the messaging system
+     * @throws MessageException if the service method and messaging model are not valid.
+     */
+    private void validateMessagingModel(String serviceMethod, String messagingSystem) throws MessageException {
+        MessageInfrastructurePropertiesFileParser parser = new MessageInfrastructurePropertiesFileParser();
+        MessagingModel messagingModel = parser.getMessagingModel(messagingSystem);
+        if (messagingModel == MessagingModel.POINT_TO_POINT) {
+            if (serviceMethod.equals("publish")) {
+                throw new MessageException("Point-to-point messages cannot use the publish method.");
+            }
+        }
+        if (messagingModel == MessagingModel.PUBLISH_SUBSCRIBE) {
+            if (!serviceMethod.equals("publish")) {
+                throw new MessageException(String.format("Publish-subscribe messages cannot use the %s method.", serviceMethod));
+            }
+        }
     }
 }
