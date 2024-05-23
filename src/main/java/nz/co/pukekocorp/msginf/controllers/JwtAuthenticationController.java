1a7eda0e9d5107f734c8e8de8ed0d7d7a6d62a4d
Alisdair Hamblyn <ahamblyn@gmail.com>
2024-05-24 10:26:48 +1200

    Store users in mysql db

--- src/main/java/nz/co/pukekocorp/msginf/controllers/JwtAuthenticationController.java
+++ src/main/java/nz/co/pukekocorp/msginf/controllers/JwtAuthenticationController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -66,15 +65,4 @@ public class JwtAuthenticationController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jwtError);
         }
     }
-
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
-    }
-
 }
